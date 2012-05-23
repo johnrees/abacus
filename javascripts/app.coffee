051a0611bf8ceacea0a682e82bf43722b59c5a55
John Rees <john@bitsushi.com>
2012-05-23 08:34:47 +0100

    unbind click

--- javascripts/app.coffee
+++ javascripts/app.coffee
@@ -1,33 +1,34 @@
-jQuery ->
+moveBead = (e) ->
+  $('.bead').unbind 'click', moveBead
+  col = $(this).data('col')
+  parent = $(this).parents('div')
 
-  # ua = navigator.userAgent
-  # # event = (ua.match(/iPad/i)) ? "touchstart" : "click"
-  # event = 'click'
+  parent.find(".bead[data-col=#{col}]").addClass('active')
+  $(".bead").bind 'click', moveBead
+  $(this).removeClass('active')
+  $(this).unbind 'click', moveBead
+
+  total = 0
+  col = 0
+
+  while col <= $('.row .bead').length
+    $(".earth .bead[data-col=#{col}]").each ->
+      if $(this).hasClass('active')
+        total+= Math.pow(10,col)
+      else return false
+    $($(".heaven .bead[data-col=#{col}]").get().reverse()).each ->
+      if $(this).hasClass('active')
+        total+= Math.pow(10,col) * 5
+      else return false
+    col++
+
+  $('.total').text String(total).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
+
+jQuery ->
 
   $('.heaven .row:not(:last) .bead, .earth .row:not(:first) .bead')
   .addClass('active')
 
   $('.total').text(0)
 
-  $(".bead").bind 'click', (e) ->
-    col = $(this).data('col')
-    parent = $(this).parents('div')
-
-    parent.find(".bead[data-col=#{col}]").addClass('active')
-    $(this).removeClass('active')
-
-    total = 0
-    col = 0
-
-    while col <= $('.row .bead').length
-      $(".earth .bead[data-col=#{col}]").each ->
-        if $(this).hasClass('active')
-          total+= Math.pow(10,col)
-        else return false
-      $($(".heaven .bead[data-col=#{col}]").get().reverse()).each ->
-        if $(this).hasClass('active')
-          total+= Math.pow(10,col) * 5
-        else return false
-      col++
-
-    $('.total').text String(total).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
+  $(".bead").bind 'click', moveBead
--- javascripts/app.js
+++ javascripts/app.js
@@ -1,35 +1,41 @@
 (function() {
+  var moveBead;
+
+  moveBead = function(e) {
+    var col, parent, total;
+    $('.bead').unbind('click', moveBead);
+    col = $(this).data('col');
+    parent = $(this).parents('div');
+    parent.find(".bead[data-col=" + col + "]").addClass('active');
+    $(".bead").bind('click', moveBead);
+    $(this).removeClass('active');
+    $(this).unbind('click', moveBead);
+    total = 0;
+    col = 0;
+    while (col <= $('.row .bead').length) {
+      $(".earth .bead[data-col=" + col + "]").each(function() {
+        if ($(this).hasClass('active')) {
+          return total += Math.pow(10, col);
+        } else {
+          return false;
+        }
+      });
+      $($(".heaven .bead[data-col=" + col + "]").get().reverse()).each(function() {
+        if ($(this).hasClass('active')) {
+          return total += Math.pow(10, col) * 5;
+        } else {
+          return false;
+        }
+      });
+      col++;
+    }
+    return $('.total').text(String(total).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+  };
 
   jQuery(function() {
     $('.heaven .row:not(:last) .bead, .earth .row:not(:first) .bead').addClass('active');
     $('.total').text(0);
-    return $(".bead").bind('click', function(e) {
-      var col, parent, total;
-      col = $(this).data('col');
-      parent = $(this).parents('div');
-      parent.find(".bead[data-col=" + col + "]").addClass('active');
-      $(this).removeClass('active');
-      total = 0;
-      col = 0;
-      while (col <= $('.row .bead').length) {
-        $(".earth .bead[data-col=" + col + "]").each(function() {
-          if ($(this).hasClass('active')) {
-            return total += Math.pow(10, col);
-          } else {
-            return false;
-          }
-        });
-        $($(".heaven .bead[data-col=" + col + "]").get().reverse()).each(function() {
-          if ($(this).hasClass('active')) {
-            return total += Math.pow(10, col) * 5;
-          } else {
-            return false;
-          }
-        });
-        col++;
-      }
-      return $('.total').text(String(total).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
-    });
+    return $(".bead").bind('click', moveBead);
   });
 
 }).call(this);
